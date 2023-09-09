da6f8817a737cb318441caaf322bc0d47791e498
Keun Woo Kim <115262940+KWKIM128@users.noreply.github.com>
2023-09-09 21:03:56 +0100

    Update nifti2jpeg.py

--- nifti2jpeg.py
+++ nifti2jpeg.py
@@ -2,22 +2,28 @@ import numpy as np
 import nibabel as nib
 import os
 from PIL import Image
+from tqdm import tqdm
 
 def nifti2jpeg(in_path, num_slices, out_path):
     """
-    Input Parameter:
-        in_path: folder where nifti file is located
-        nun_slices: number of slices in the scan
-        out_path: folder where the jpeg will be saved
-    """
+    This function converts one nifti to jpeg series
     
-    nifti = nib.load(in_path).get_fdata()
-    nifti = nifti.astype(np.uint8)
-    nifti_name = os.path.basename(in_path).split(".")[0]
+    Input Parameter:
+        in_path: path to nifti file
+        nun_slices: number of slices in the scan
+        out_path: path to output
+    """
+
+    nifti_name = os.path.basename(in_path).split(".")[0] # gets name of the nifti file
+    
+    nifti = nib.load(in_path).get_fdata() # reads/load nifti file
+    new_image = nifti.astype(float)
+    scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255.0 # rescaling the image
+    nifti_arr = np.uint8(scaled_image)
 
     
-    for i in range(num_slices):
+    for i in tqdm(range(num_slices)):
         name = nifti_name + '_' + str(i)
-        image = Image.fromarray(nifti[:,:,i])
+        image = Image.fromarray(nifti_arr[:,:,i])
     
         image.save(out_path + '/' + name + '.jpeg')
