bafd0697e5f0ee2b53571276b5a78d870a13302c
Keun Woo Kim <115262940+KWKIM128@users.noreply.github.com>
2023-10-11 13:23:39 +0100

    Update nifti2image.py

--- nifti2image.py
+++ nifti2image.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 from tqdm import tqdm
 
-def mask_nifti2image(in_path, num_slices, out_path, png=False):
+def nifti2image(in_path, num_slices, out_path, png=False):
     """
     This function converts one nifti to jpeg/png series
     
@@ -31,9 +31,9 @@ def mask_nifti2image(in_path, num_slices, out_path, png=False):
         image = Image.fromarray(nifti_arr[:,:,i])    
         image.save(out_path + '/' + name + file_format)
 
-def nifti2image(in_path, num_slices, out_path, png=False):
+def binaraymask_nifti2image(in_path, num_slices, out_path, png=False):
     """
-    This function converts one nifti to jpeg/png series
+    This function converts one nifti to binary jpeg/png series
     
     Input Parameter:
         in_path: path to nifti file
@@ -48,13 +48,12 @@ def nifti2image(in_path, num_slices, out_path, png=False):
     nifti_name = os.path.basename(in_path).split(".")[0] # gets name of the nifti file
     
     nifti = nib.load(in_path).get_fdata() # reads/load nifti file
-    new_image = nifti.astype(float)
-    scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255.0 # rescaling the image
-    nifti_arr = np.uint8(scaled_image)
-
-    
     for i in tqdm(range(num_slices)):
-        name = nifti_name + '_' + str(i)
-        image = Image.fromarray(nifti_arr[:,:,i])
-        image[image[:,:,i}>0] = 1
-        image.save(out_path + '/' + name + file_format)
+        image = nifti[:,:,i]
+        image[image>0] = 255
+        new_image = image.astype(float)
+        nifti_arr = np.uint8(new_image)
+        
+        name =  'test_' + str(i)
+        image = Image.fromarray(nifti_arr)
+        image.save('F:/Automated_BE/trial/' + name + '.jpeg')
